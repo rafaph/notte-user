2c7cbade4af053a6f139f241285786e8e4fde292
Raphael Castro <rafaphcastro@gmail.com>
2023-07-20 20:03:49 -0300

    refactor: disable logs func

--- test/helpers/disable-logs.ts
+++ test/helpers/disable-logs.ts
@@ -1,12 +1,14 @@
 import { Logger } from "@nestjs/common";
-import { get } from "lodash";
+import { get, has } from "lodash";
 
 export function disableLogs<T = object>(sut: T): void {
-  const logger = get(sut, "logger") as unknown as Logger;
+  if (has(sut, "logger")) {
+    const logger = get(sut, "logger") as unknown as Logger;
 
-  jest.spyOn(logger, "log").mockReturnValue();
-  jest.spyOn(logger, "error").mockReturnValue();
-  jest.spyOn(logger, "debug").mockReturnValue();
-  jest.spyOn(logger, "verbose").mockReturnValue();
-  jest.spyOn(logger, "warn").mockReturnValue();
+    jest.spyOn(logger, "log").mockReturnValue();
+    jest.spyOn(logger, "error").mockReturnValue();
+    jest.spyOn(logger, "debug").mockReturnValue();
+    jest.spyOn(logger, "verbose").mockReturnValue();
+    jest.spyOn(logger, "warn").mockReturnValue();
+  }
 }
